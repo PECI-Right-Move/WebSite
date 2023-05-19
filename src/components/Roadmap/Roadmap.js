7a38163569f231d84453e806ba7b1a9b95b3ec40
bea-rodrigues <bea.rodrigues@ua.pt>
2023-05-19 20:26:31 +0100

    Reverted to old roadmap

---
+++ src/components/Roadmap/Roadmap.js
@@ -0,0 +1,98 @@
+import "./Roadmap.css";
+import React from "react";
+
+import { ReactComponent as DevelopmentIcon } from "../../assets/img/icons/common/github.svg";
+import { ReactComponent as ResearchIcon } from "../../assets/img/icons/common/google.svg";
+
+import roadmapELements from "./RoadmapElements";
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
+
+class Roadmap extends React.Component {
+  state = {};
+  render() {
+    const getMilestoneColor = (milestone) => {
+        switch (milestone) {
+          case "1":
+            return "#4947cf23";
+          case "2":
+            return "#f500561c";
+          case "3":
+            return "#038ea118";
+          default:
+            return "#212121";
+        }
+      };
+    let developmentIconStyles = {
+      color: "#172b4d",
+      background: "white",
+    };
+    let researchIconStyles = {
+      color: "#172b4d",
+      background: "white",
+    };
+    return (
+      <>
+        <main>
+          <h1 className="roadmapTitle">WORKPLAN</h1>
+          <VerticalTimeline lineColor={"#172b4d"} animate={true}>
+            {roadmapELements.map((element) => {
+              let isDevelopmentIcon = element.icon === "development";
+
+              return (
+                <VerticalTimelineElement
+                  key={element.id}
+                  date={element.date}
+                  dateClassName="date"
+                  iconStyle={
+                    isDevelopmentIcon
+                      ? developmentIconStyles
+                      : researchIconStyles
+                  }
+                  icon={
+                    isDevelopmentIcon ? <DevelopmentIcon /> : <ResearchIcon />
+                  }
+                  className={`milestone-${element.milestone}`}
+                  contentStyle={{background: getMilestoneColor(element.milestone)}}
+                >
+                  <h3 className="vertical-timeline-element-title">
+                    {element.title}
+                  </h3>
+                  <h5 className="vertical-timeline-element-subtitle">
+                    {element.location}
+                  </h5>
+                  <p id="description">{element.description}</p>
+                  {element.buttons.map((button) => {
+                    
+                    return (
+                        button.buttonText !== undefined &&
+                        button.buttonText !== null &&
+                        button.buttonText !== "" &&
+                      <a
+                        className={`mybutton ${
+                          isDevelopmentIcon
+                            ? "developmentButton"
+                            : "researchButton"
+                        }`}
+                        href="/"
+                      >
+                        {button.buttonText}
+                      </a>
+                    );
+                  })}
+                </VerticalTimelineElement>
+              );
+            })}
+          </VerticalTimeline>
+        </main>
+      </>
+    );
+  }
+}
+
+export default Roadmap;
--- src/views/pages/Workplan.js
+++ src/views/pages/Workplan.js
@@ -1,69 +1,57 @@
-import React from "react";
-import "assets/vendor/nucleo/css/nucleo.css";
-
-// components
-import DemoNavbar from "components/Navbars/DemoNavbar.js";
-import Roadmap2 from "components/RoadMap2.0/App";
-
-// reactstrap components
-import { Card, CardBody } from "reactstrap";
-
-class Workplan extends React.Component {
-  componentDidMount() {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-    this.refs.main.scrollTop = 0;
-  }
-
-  render() {
-    return (
-      <>
-        <DemoNavbar />
-        <main className="profile-page" ref="main">
-          <section className="section-profile-cover section-shaped my-0 position-relative">
-            {" "}
-            {/* Circles background */}{" "}
-            <div className="shape shape-style-1 shape-default alpha-4 positon-relative">
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-            </div>{" "}
-            {/* SVG separator */}{" "}
-            <div className="separator separator-bottom separator-skew">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-              >
-                <polygon
-                  className="fill-white"
-                  points="2560 0 2560 100 0 100"
-                />
-              </svg>{" "}
-            </div>{" "}
-          </section>{" "}
-        </main>{" "}
-        <div className="align-items-center"
-        style={{  margin: "auto",
-          width: "50%",
-          }}>
-          <Card
-            className="shadow border-0"
-            style={{ height: "90vh", width: "50vw" }}
-          >
-            <CardBody>
-              <Roadmap2 />
-            </CardBody>
-          </Card>
-        </div>
-      </>
-    );
-  }
-}
-
+import React from "react"; 
+import "assets/vendor/nucleo/css/nucleo.css"; 
+// reactstrap components 
+import {Container} from "reactstrap"; 
+ 
+// components 
+import DemoNavbar from "components/Navbars/DemoNavbar.js"; 
+import Roadmap from "components/Roadmap/Roadmap"; 
+ 
+class Workplan extends React.Component { 
+  componentDidMount() { 
+    document.documentElement.scrollTop = 0; 
+    document.scrollingElement.scrollTop = 0; 
+    this.refs.main.scrollTop = 0; 
+  } 
+  render() { 
+    return ( 
+      <> 
+        <DemoNavbar /> 
+        <main className="profile-page" ref="main"> 
+          <section className="section-profile-cover section-shaped my-0 position-relative"> 
+            {/* Circles background */} 
+            <div className="shape shape-style-1 shape-default alpha-4 positon-relative"> 
+              <span /> 
+              <span /> 
+              <span /> 
+              <span /> 
+              <span /> 
+            </div> 
+            {/* SVG separator */} 
+            <div className="separator separator-bottom separator-skew"> 
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                preserveAspectRatio="none" 
+                version="1.1" 
+                viewBox="0 0 2560 100" 
+                x="0" 
+                y="0" 
+              > 
+                <polygon 
+                  className="fill-white" 
+                  points="2560 0 2560 100 0 100" 
+                /> 
+              </svg> 
+            </div> 
+          </section> 
+        </main> 
+ 
+        <Container> 
+          <Roadmap/> 
+        </Container> 
+      </> 
+    ); 
+  } 
+} 
+ 
 export default Workplan;
